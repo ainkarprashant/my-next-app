2bd1d9a840eb5a2140cd9c8d754c49efb25bf57f
Prashant Ainkar <ainkarprashant@gmail.com>
2025-10-18 22:40:20 +0530

    contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,6 +3,10 @@ export default function ContactPage() {
     <main style={{ padding: '2rem' }}>
       <h2>Contact Page</h2>
       <p>This is the Contact page. You can differentiate this page by its unique content.</p>
+      <p>This is the Contact page. You can differentiate this page by its unique content.</p>
+      <p>This is the Contact page. You can differentiate this page by its unique content.</p>
+      <p>This is the Contact page. You can differentiate this page by its unique content.</p>
+      <p>This is the Contact page. You can differentiate this page by its unique content.</p>
     </main>
   );
 }
