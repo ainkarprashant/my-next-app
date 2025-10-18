803814ef46491ec2dcd38d077d91c4fa09d4b163
Prashant Ainkar <ainkarprashant@gmail.com>
2025-10-18 22:39:50 +0530

    about us page

--- app/about/page.js
+++ app/about/page.js
@@ -3,6 +3,10 @@ export default function About() {
         <div>
             <h1>About Us</h1>
             <p>We are a company dedicated to providing the best services to our customers.</p>
+            <p>We are a company dedicated to providing the best services to our customers.</p>
+            <p>We are a company dedicated to providing the best services to our customers.</p>
+            <p>We are a company dedicated to providing the best services to our customers.</p>
+            <p>We are a company dedicated to providing the best services to our customers.</p>
         </div>
     );
 }
